4b22a3d7a09742347450bcbcfdc5d95a1fdd053c
Pe7r0v14 <davydovpetrvladimirovich@gmail.com>
2024-07-28 22:57:34 +0800

    iCommit

--- scripts/main.py
+++ scripts/main.py
@@ -11,15 +11,15 @@ from work_in_db import vision_data as vis_d
 
 
 def welcome():
-    first_user_enter = ps('Hello!What do you want?:\n1)Add product\n2)Delete product\n3)Update product\n4)Vision products\n>>')
-    if first_user_enter == 'Add product' or 1:
-        enter_add_product()
-    elif first_user_enter == 'Delete product' or 2:
-        enter_delete_product()
-    elif first_user_enter == 'Update product' or 3:
-        enter_update_product()
-    elif first_user_enter == 'Vision products' or 4:
+    first_user_enter = pi('Hello!What do you want?:\n1)Add product\n2)Delete product\n3)Update product\n4)Vision products\n>>')
+    if first_user_enter == 4:
         enter_vision_product()
+    elif first_user_enter == 2:
+        enter_delete_product()
+    elif first_user_enter ==3:
+        enter_update_product()
+    elif first_user_enter == 1:
+        enter_add_product()
     else:
         pass
 
@@ -29,20 +29,30 @@ def welcome():
     #vis_d()
 
 def enter_add_product():
-    #add_d()
-    pass
+    print('ADD!')
+    print("Hello, user, enter the data product!")
+    categories = ps('Enter categories product\n>>')
+    name_product = ps('Enter name product\n>>')
+    code_product = pi('Enter code product\n>>')
+    data_start = pi('Enter date start\n>>')
+    finished = pi('Enter how month\n>>')
+    value = pi('Enter value product\n>>')
+    add_d(categories,name_product,code_product,data_start,finished,value)
+    print('Product added!')
 
 def enter_delete_product():
     #del_d()
+    print('DELETE!')
     pass
 
 def enter_update_product():
+    print('UPDATE!')
     #up_d()
     pass
 
 def enter_vision_product():
-    #vis_d
-    pass
+    print('VISION!')
+    vis_d()
 
 
 welcome()
